227b76395a70efcc425b29abb8166457b33bbe8c
Éva Herőczi <vicaheroczi@gmail.com>
2020-10-04 16:10:40 +0200

    dataexport

--- exceles/exceles/Form1.cs
+++ exceles/exceles/Form1.cs
@@ -9,6 +9,10 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Reflection;
+using System.Drawing.Text;
+using System.Runtime.Remoting.Messaging;
+using System.Diagnostics;
+using Microsoft.Office.Core;
 
 namespace exceles
 {
@@ -40,7 +44,7 @@ namespace exceles
                 xlWB = xlApp.Workbooks.Add(Missing.Value);
                 xlSheet = xlWB.ActiveSheet;
 
-                //CreateTable();
+                CreateTable();
 
                 xlApp.Visible = true;
                 xlApp.UserControl = true;
@@ -56,8 +60,73 @@ namespace exceles
                 xlWB = null;
                 xlApp = null;
             }
-            
-
         }
+
+        private void CreateTable()
+        {
+            string[] headers = new string[] 
+            {
+            "Kód",
+            "Eladó",
+            "Oldal",
+            "Kerület",
+            "Lift",
+            "Szobák száma",
+            "Alapterület (m2)",
+            "Ár (mFt)",
+            "Négyzetméter ár (Ft/m2)"
+            };
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i + 1] = headers[i];
+            }
+
+            object[,] values = new object[Flats.Count, headers.Length];
+
+            int counter = 0;
+            foreach (var item in Flats)
+            {
+                string lift = "";
+                if (item.Elevator == true)
+                {
+                    lift = "Van";
+                }
+                else
+                {
+                    lift = "Nincs";
+                }
+                values[counter, 0] = item.Code;
+                values[counter, 1] = item.Vendor;
+                values[counter, 2] = item.Side;
+                values[counter, 3] = item.District;
+                values[counter, 4] = lift;
+                values[counter, 5] = item.NumberOfRooms;
+                values[counter, 6] = item.FloorArea;
+                values[counter, 7] = item.Price / item.FloorArea;
+                values[counter, 8] = xlSheet.get_Range(
+                                                       GetCell(2, 1),
+                                                       GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+                counter++;
+            }
+        }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+
     }
 }
