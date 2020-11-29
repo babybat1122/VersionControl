6604a90c257d7c6189910751b088c1846514c7a1
Éva Herőczi <vicaheroczi@gmail.com>
2020-11-29 17:47:27 +0100

    jatekter

---
+++ UnitTestExample/UnitTestExample.Test/PasswordControllerTestFixture.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestExample.Test
+{
+    class PasswordControllerTestFixture
+    {
+        [Test,
+            TestCase("abcd", true),
+            TestCase("ABCD1234", true),
+            TestCase("abcd1234", true),
+            TestCase(len)
+    }
+}
